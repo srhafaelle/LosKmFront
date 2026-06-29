040573fe8f69c5900793c55c4ebcfef02c370b9b
jaraycode <jonasaray12@gmail.com>
2026-06-29 10:37:10 -0400

    config: configuraciones base para el proyecto, entre las rutas, estilos, base de la aplicación, entre otros

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Landing } from '@/layouts/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Landing,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@/pages/landing/principal/principal').then((p) => p.Principal),
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('@/pages/landing/about/about').then((p) => p.About),
+      },
+      {
+        path: 'contact',
+        loadComponent: () => import('@/pages/landing/contact/contact').then((p) => p.Contact),
+      },
+    ],
+  },
+];
