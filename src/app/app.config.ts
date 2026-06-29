0dfae9ec5bee935ea87173e90243d11deb571931
jaraycode <jonasaray12@gmail.com>
2026-06-29 19:15:50 -0400

    config: incluyendo las rutas sobre el apartado de autneticación y configuración sobre tamaño de los íconos y colores predeterminados

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideNgIconsConfig, withExceptionLogger } from '@ng-icons/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+        provideHttpClient(withInterceptors([])),
+        provideNgIconsConfig({ size: '1.5em', color: 'var(--text)' }, withExceptionLogger()),
+    ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,36 @@
 import { Routes } from '@angular/router';
 import { Landing } from '@/layouts/landing/landing';
+import { Auth } from '@/layouts/auth/auth';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: Landing,
-    children: [
-      {
+    {
         path: '',
-        loadComponent: () => import('@/pages/landing/principal/principal').then((p) => p.Principal),
-      },
-      {
-        path: 'about',
-        loadComponent: () => import('@/pages/landing/about/about').then((p) => p.About),
-      },
-      {
-        path: 'contact',
-        loadComponent: () => import('@/pages/landing/contact/contact').then((p) => p.Contact),
-      },
-    ],
-  },
+        component: Landing,
+        children: [
+            {
+                path: '',
+                loadComponent: () =>
+                    import('@/pages/landing/principal/principal').then((p) => p.Principal),
+            },
+            {
+                path: 'about',
+                loadComponent: () => import('@/pages/landing/about/about').then((p) => p.About),
+            },
+            {
+                path: 'contact',
+                loadComponent: () =>
+                    import('@/pages/landing/contact/contact').then((p) => p.Contact),
+            },
+        ],
+    },
+    {
+        path: 'auth',
+        component: Auth,
+        children: [
+            {
+                path: 'login',
+                loadComponent: () => import('@/pages/auth/login/login').then((p) => p.Login),
+            },
+        ],
+    },
 ];
