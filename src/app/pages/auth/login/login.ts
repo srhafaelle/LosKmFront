262db76804d45dfc3e50b817afa9889aa7b13ab3
jaraycode <jonasaray12@gmail.com>
2026-07-08 17:39:22 -0400

    refactor: se refactorizó el componente de login para seguir buenas prácticas de aplicaciones en producción

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,31 +1,28 @@
 import { Authentication } from '@/core/auth/authentication';
 import { ButtonComponent } from '@/shared/ui/button-component/button-component';
 import { CardComponent } from '@/shared/ui/card-component/card-component';
-import { InputComponent } from '@/shared/ui/input-component/input-component';
-import { getControlError } from '@/shared/utils';
+import { formatErrors, getControlError } from '@/shared/utils';
 import { NgOptimizedImage } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { bootstrapKey, bootstrapPerson } from '@ng-icons/bootstrap-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import { finalize } from 'rxjs';
 
 @Component({
     selector: 'app-login',
-    imports: [
-        InputComponent,
-        CardComponent,
-        ButtonComponent,
-        NgOptimizedImage,
-        ReactiveFormsModule,
-    ],
+    imports: [CardComponent, ButtonComponent, NgOptimizedImage, ReactiveFormsModule, NgIcon],
     templateUrl: './login.html',
     styleUrl: './login.css',
+    viewProviders: [provideIcons({ bootstrapPerson, bootstrapKey })],
 })
 export class Login {
     // Inyección de dependencias: Formularios y Servicio de autenticación
-    #formBuilder = inject(NonNullableFormBuilder);
-    #authService = inject(Authentication);
-    #router = inject(Router);
+    readonly #formBuilder = inject(NonNullableFormBuilder);
+    readonly #authService = inject(Authentication);
+    readonly #router = inject(Router);
 
     // Formulario para el caso de uso de inicio de sesión.
     loginForm = this.#formBuilder.group({
@@ -49,18 +46,19 @@ export class Login {
     }
 
     // Estados globales de la página
-    globalError = signal<string | null>(null);
+    globalError = signal<string[]>([]);
     isLoading = signal<boolean>(false);
 
     // Eventos que se pueden realizar sobre los componentes de la página, para este caso se prioriza en el inicio de sesión sobre el formulario.
     logIn() {
         if (this.loginForm.invalid) {
             this.loginForm.markAllAsTouched();
+            console.table(this.loginForm.getRawValue());
             return;
         }
 
         this.isLoading.set(true);
-        this.globalError.set(null);
+        this.globalError.set([]);
 
         this.#authService
             .login(this.loginForm.getRawValue())
@@ -73,9 +71,11 @@ export class Login {
                 next: () => {
                     this.#router.navigate(['/dashboard/index']);
                 },
-                error: (error) => {
-                    console.error(`Error al iniciar sesión: ${error}`);
-                    this.globalError.set(`Credenciales inválidas. Por favor, intente nuevamente.`);
+                error: (error: HttpErrorResponse) => {
+                    const formattedErrors = formatErrors(error.error?.errors);
+                    this.globalError.set(
+                        formattedErrors.length ? formattedErrors : ['Credenciales invalidas'],
+                    );
                 },
             });
     }
