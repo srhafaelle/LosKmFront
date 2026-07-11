28d02522259a193516043bd9fdca5295d4912f0e
jaraycode <jonasaray12@gmail.com>
2026-07-11 17:01:51 -0400

    config: agregando el paquete angularx-qr que permite integrar el código QR que será necesario para buscar información con mayor facilidad
    config: agregando la capacidad de identificar formatos en español

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,10 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideNgIconsConfig, withExceptionLogger } from '@ng-icons/core';
 import { apiPrefixInterceptorInterceptor, withBearerInterceptor } from '@/core/auth';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -16,5 +20,6 @@ export const appConfig: ApplicationConfig = {
             withInterceptors([apiPrefixInterceptorInterceptor, withBearerInterceptor]),
         ), // TODO: PONER EL INTERCEPTOR WITH_BEARER_INTERCEPTOR CUANDO SE NECESITE PROBAR LA PETICION SOBRE ENDPOINTS PROTEGIDOS
         provideNgIconsConfig({ size: '1.5em', color: 'var(--text)' }, withExceptionLogger()),
+        { provide: LOCALE_ID, useValue: 'es' },
     ],
 };
